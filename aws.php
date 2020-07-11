0b806c6de970eeccd4cf9b299724d489bb87d9a8
Kwynn <buessresume@gmail.com>
2020-07-11 02:55:57 -0400

    tiny bit of cleanup

--- aws.php
+++ aws.php
@@ -8,27 +8,12 @@ function getAWSPath() {
     if (!isAWS()) return __DIR__ . '/..' . '/cpu/get/get.php';
 }
 
-class aws_periodic_metrics {
-    
-    public function __construct() {
-	self::getAWS();
-    }
-
-private static function getAWS() {
-    $narr = $carr = [];
+function getAWS() { 
+    $ca = $na = [];
     
     $ca = aws_cpu::awsMRegGet(0   , ['cpu'], 1);
     $na = aws_cpu::awsMRegGet(1/24, ['net'], 1);    
-    
-    // aws_cpu::cliGet('2020-06-21', '2020-06-24', 86400, 'net', $narr);
-    // aws_cpu::cliGet('2020-06-21', '2020-06-24', 86400, 'cpu', $carr);
-    return;
-}
 
-private static function getcpu() {
+    return; // note that I'm not returning anything yet; I'll do so later.
     
 }
-
-}
-
-function getAWS() { new aws_periodic_metrics(); }
